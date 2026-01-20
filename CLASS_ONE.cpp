88ed51a7fe662edf67c26b5e32bd8bae1273d1cf
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-01-20 20:09:56 +0500

    Update CLASS_ONE.cpp

--- CLASS_ONE.cpp
+++ CLASS_ONE.cpp
@@ -2,7 +2,22 @@
 using namespace std;                    // we use it so we don't have to write std again and again
 
 class Bankaccount{
+
     string name;                        
     int account_number;
     double balance;
+
+public:
+    void details(string N, int AN, double B){
+        name = N;
+        account_number = AN;
+        balance = B;
+    }
+
+
+
 };
+
+int main(){
+    Bankaccount Girdhari("Girdhari lal", 1012021, 1200.00 );
+}
