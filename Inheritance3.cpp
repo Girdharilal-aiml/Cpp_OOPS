be3b63b82064a069bf3a30de8d8e4f796e459587
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-01-20 23:38:42 +0500

    Update Inheritance3.cpp

--- Inheritance3.cpp
+++ Inheritance3.cpp
@@ -1,2 +1,24 @@
 // Multiple Inheritance
 
+#include "iostream"
+using namespace std;
+
+class Cricketer{
+public:
+    int runs;
+    string team;
+};
+
+class Engineer{
+public:
+    int experience;
+};
+
+class phodu : public Cricketer,Engineer{
+public:
+
+};
+
+int main(){
+    
+}
