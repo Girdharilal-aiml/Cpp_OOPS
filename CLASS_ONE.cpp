e24525ef18a8f184c972ef6e64dc1d99c7e80bcd
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-01-20 20:05:59 +0500

    Create CLASS_ONE.cpp

---
+++ CLASS_ONE.cpp
@@ -0,0 +1,8 @@
+#include "iostream"
+using namespace std;                    // we use it so we don't have to write std again and again
+
+class Bankaccount{
+    string name;                        
+    int account_number;
+    double balance;
+};
