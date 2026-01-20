84ea1e781bacf5fcd10c54d147c3ce9c8a3914f3
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-01-20 21:58:06 +0500

    Update Inheritance.cpp

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -1,17 +1,35 @@
 #include "iostream"
 using namespace std;
 
-class Scooty{                               // Parent Class
+class Vehicle{                               // Parent Class
 public:
     int TopSpeed;
     float Mileage;
+    string fuel;
+
+private:
+    int bootspace;
 };
 
-class Bike : public Scooty{                        // class Bike extend to Scooty      ---> Child/Derived Class
+class Bike : public Vehicle{                       // class Bike extend to Vehicle    ---> Child/Derived Class
 public:
     int gears;
 };
 
+class Car : public Vehicle{                        // Child/Derived Class
+public:
+    bool sunroof;
+};
+
+class Truck : public Vehicle{                       // Child/Derived Class
+public:
+    string Pickup_limit;
+};
+
 int main(){
+    Bike B1;
+    B1.TopSpeed = 120;
+    B1.Mileage = 30.5;
+    B1.gears = 5;
 
 }
