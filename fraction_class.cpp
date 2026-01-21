a5c1985e32f25c10c03ee60ef13a1366a524aba8
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-01-21 17:38:41 +0500

    Update fraction_class.cpp

--- fraction_class.cpp
+++ fraction_class.cpp
@@ -5,8 +5,16 @@ class Fraction{
 public:
     int num;
     int deno;
+
     Fraction(int num,int deno){
         this->num = num;
         this->deno = deno;
     }
+
+    void display(int num, int deno){
+        cout<<num<<" / "<<deno<<endl;
+    }
+
 };
+
+
