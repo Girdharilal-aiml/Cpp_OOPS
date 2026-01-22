db6ae49aff941f59dfb14376172d1235922bd467
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-01-22 21:15:09 +0500

    Update Q1.cpp

--- PRACTICE Q_A/Q1.cpp
+++ PRACTICE Q_A/Q1.cpp
@@ -1,1 +1,30 @@
-// Question 1
+/*
+Problem 1 (VERY EASY)
+
+Create a Student class with:
+
+name
+
+marks
+Function:
+
+display() → prints name & marks 
+*/
+
+#include "iostream"
+using namespace std;
+
+class Student{
+    string name="Girdhari lal";
+    int marks=99;
+
+public:
+    void display(){
+        cout<<name<<" "<<marks<<endl;
+    }
+};
+
+int main(){
+    Student S;
+    S.display();
+}
