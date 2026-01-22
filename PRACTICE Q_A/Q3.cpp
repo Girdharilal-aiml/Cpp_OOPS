d9b26aea5dade5979e03562fdfd09c85e51f9e7b
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-01-22 21:32:09 +0500

    Update Q3.cpp

--- PRACTICE Q_A/Q3.cpp
+++ PRACTICE Q_A/Q3.cpp
@@ -16,16 +16,21 @@ Display them
 using namespace std;
 
 class Student{
-    string name="Girdhari lal";
-    int marks=99;
-
 public:
+    string name;
+    int marks;
+
+    Student(string n,int m){
+        name = n;
+        marks = m;
+    }
+
     void display(){
         cout<<name<<" "<<marks<<endl;
     }
 };
 
 int main(){
-    Student S;
+    Student S("Girdhari lal", 99);
     S.display();
 }
