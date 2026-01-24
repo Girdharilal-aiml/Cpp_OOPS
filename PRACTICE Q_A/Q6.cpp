28d0f6762e023ec28b709db6598f3c6df52d7e07
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-01-24 16:07:46 +0500

    Create Q6.cpp

---
+++ PRACTICE Q_A/Q6.cpp
@@ -0,0 +1,19 @@
+/*
+Your Turn (IMPORTANT PRACTICE)
+
+Create BankAccount:
+
+private: balance
+
+constructor
+
+deposit() (no negative)
+
+withdraw() (no overdraft)
+
+showBalance()
+*/
+
+#include"iostream"
+using namespace std;
+
