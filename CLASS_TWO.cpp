26916e600a16175118c57840c92bd967beded84c
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-01-30 19:30:35 +0500

    Update CLASS_TWO.cpp

--- CLASS_TWO.cpp
+++ CLASS_TWO.cpp
@@ -1,12 +1,20 @@
 #include"iostream"
 #include"string"
 #include"cctype"
+using namespace std;
 
 class NameMatcher{
 private:
+    string name1;
+    string name2;
+    int remain1;
+    int remain2;
+
+public:
 
 };
 
 int main(){
+    string name1, name2;
     
 }
