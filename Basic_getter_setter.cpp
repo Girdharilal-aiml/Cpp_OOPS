4da1ba052300b9b7d8dd71e1d3eea5463590b2f1
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-01-30 18:54:33 +0500

    Update Basic_getter_setter.cpp

--- Basic_getter_setter.cpp
+++ Basic_getter_setter.cpp
@@ -16,5 +16,12 @@ public:
 
 int main(){
     Student S1;
-   
+    S1.setname("Girdhari lal");
+    S1.setage(19);
+    S1.setcourse("OOP LAB");
+    S1.setmarks(99.9);
+    cout<<S1.getname()<<endl;
+    cout<<S1.getcourse()<<endl;
+    cout<<S1.getage()<<endl;
+    cout<<S1.getmarks()<<endl;
 }
