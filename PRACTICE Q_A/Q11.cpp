9e7d5a27a8971a601bd732fb66ea5048dddeea73
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-01-31 19:49:04 +0500

    Create Q11.cpp

---
+++ PRACTICE Q_A/Q11.cpp
@@ -0,0 +1,17 @@
+/*
+3. Create a class called WaterBottle with the following attributes:
+   - company (made by)
+   - color
+   - water capacity stored in:
+     - liters (l)
+     - milliliters (ml)
+
+   Create variables and methods for the class.
+   Methods should include getters and setters.
+
+   Add an additional method that:
+   - Asks the user how much water was drunk (in ml)
+   - Updates the remaining water capacity (both l and ml)
+
+   Demonstrate the functionality in the main method.
+*/
