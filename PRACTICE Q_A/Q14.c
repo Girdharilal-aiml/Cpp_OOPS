42c7a258172ffb31d25f22cfbede29032fcafdd8
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-01-31 19:39:46 +0500

    Create Q14.c

---
+++ PRACTICE Q_A/Q14.c
@@ -0,0 +1,10 @@
+/*
+6. Based on Question 5:
+   - Separate the Employee class into:
+     * Employee.h       (class definition)
+     * Implement.cpp    (member function implementation)
+     * main.cpp         (contains main function)
+
+   Demonstrate the program by creating
+   at least 3 Employee objects.
+*/
