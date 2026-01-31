e67d01ca99940e6a131df51a810e729c2e4e3b71
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-01-31 19:49:12 +0500

    Create Q13.cpp

---
+++ PRACTICE Q_A/Q13.cpp
@@ -0,0 +1,15 @@
+/*
+5. Design an Employee class with the following functions:
+
+   - get_data():
+     Collect employee name, monthly salary,
+     and tax percentage.
+
+   - Salary_after_tax():
+     Deduct 2% tax from the salary
+     and return the remaining salary.
+
+   - update_tax_percentage():
+     Increase tax percentage (e.g., from 2% to 3%)
+     Recalculate and display updated salary after tax.
+*/
