69a9d34a622380c867b5aa41347b57cb822058fb
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-02-01 14:57:26 +0500

    Update Q9.cpp

--- PRACTICE Q_A/Q9.cpp
+++ PRACTICE Q_A/Q9.cpp
@@ -31,17 +31,13 @@ private:
     string color;
     bool Refillable;
     bool Ink_Status;
+    
 public:
-    BoardMarker(string Company, string color){
+    BoardMarker(string Company, string color, bool Refillable,bool Ink_Status){
         this->color = color;
         this->Company = Company;
-        Refillable = 0;
-        Ink_Status = 0;
-    }
-
-    void set_info(string PC, string P_col){
-        Company = PC;
-        color = P_col;
+        this->Refillable = Refillable;
+        this->Ink_Status = Ink_Status;
     }
 
     string get_com(){
@@ -53,73 +49,59 @@ public:
     }
 
     void write(){
-      int iks = Ink_Status;
-      cout<<"Is pen ink empty?(1-YES,0-NO): ";
-      cin>>iks;
 
-      if(iks == 0){
-        cout<<endl;
+      if(Ink_Status){
         cout<<"Proceed with writing."<<endl; 
-        cout<<endl;
-        Ink_Status = 1;
-      }else if(iks == 1){
+        Ink_Status = false;
+      }else if(!Ink_Status){
         cout<<"Ink Empty! writing is not possible."<<endl; 
-        cout<<endl;
-        Ink_Status = 0;
       }
     }
 
     void refill(){
-      int Refill = Refillable;
-      cout<<"Is pen Refillable?(1-YES,0-NO): ";
-      cin>>Refill;
-      cout<<endl;
 
-
-      if(Refill == 1){
-        cout<<"Ink Refilled"<<endl; 
-        cout<<endl;
+      if(Refillable){
+        cout<<"Ink Refilled!"<<endl;
         Ink_Status = 1;
-      }else if(Refill == 0){
-        cout<<"The marker cannot be refilled."<<endl; 
-        cout<<endl;
-        Ink_Status = 0;
-      }
+      }else if(!Refillable){
+        cout<<"Marker cannot be refilled!"<<endl;
+      }  
     }
 
 };
 
 int main(){
-    string com1,col1, com2,col2, com3,col3;
-    cout<<"Enter pen company name: ";
-    getline(cin, com1);
-    cout<<"Enter pen color: ";
-    getline(cin, col1);
-    cout<<"Enter pen company name: ";
-    getline(cin, com2);
-    cout<<"Enter pen color: ";
-    getline(cin, col2);
-    cout<<"Enter pen company name: ";
-    getline(cin, com3);
-    cout<<"Enter pen color: ";
-    getline(cin, col3);
     string company1, color1, company2, color2, company3, color3;
-    company1 = com1;
-    color1 = col1;
-    company2 = com2;
-    color2 = col2;
-    company3 = com3;
-    color3 = col3;
+    int Ref1, Ref2, Ref3, st1,st2,st3;
 
-    BoardMarker BM1(company1, color1);
-    BoardMarker BM2(company2, color2);
-    BoardMarker BM3(company3, color3);
+    cout<<"Enter pen company name: ";
+    getline(cin, company1);
+    cout<<"Enter pen color: ";
+    getline(cin, color1);
+    cout<<"Is pen Refillable?(1-YES,0-NO): ";
+    cin>>Ref1;
+    cin.ignore();
+    cout<<"Is pen ink empty?(1-YES,0-NO): ";
+    cin>>st1;
+    cin.ignore();
+    cout<<endl;
 
+    cout<<"Enter pen company name: ";
+    getline(cin, company2);
+    cout<<"Enter pen color: ";
+    getline(cin, color2);
+    cout<<"Is pen Refillable?(1-YES,0-NO): ";
+    cin>>Ref2;
+    cin.ignore();
+    cout<<"Is pen ink empty?(1-YES,0-NO): ";
+    cin>>st2;
+    cin.ignore();
+    cout<<endl;
 
-    BM1.set_info(company1,color1);
-    BM2.set_info(company2,color2);
-    BM3.set_info(company3,color3);
-
+    cout<<"Enter pen company name: ";
+    getline(cin, company3);
+    cout<<"Enter pen color: ";
+    
     cout<<"-----------------------------------"<<endl;
     cout<<BM1.get_com()<<" "<<BM1.get_color()<<endl;
     BM1.write();
