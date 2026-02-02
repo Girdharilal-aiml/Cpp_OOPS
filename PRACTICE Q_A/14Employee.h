959e25fb366c2119073ab101fcb4e4a4fe96ac45
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-02-02 23:00:40 +0500

    Update 14Employee.h

--- PRACTICE Q_A/14Employee.h
+++ PRACTICE Q_A/14Employee.h
@@ -4,6 +4,17 @@
 #include <iostream>
 using namespace std;
 
+class Employee {
+private:
+    string name;
+    double salary;
+    double taxPercent;      // in decimal (0.02 = 2%)
 
+public:
+    void get_data(string n, double sal);
+    double Salary_after_tax();
+    void update_tax_percentage(double newTax);
+    void display();
+};
 
 #endif
