36c014cfd30ff15d935f7405280fc8a4de4108c7
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-02-02 23:02:20 +0500

    Update 14main.cpp

--- PRACTICE Q_A/14main.cpp
+++ PRACTICE Q_A/14main.cpp
@@ -3,7 +3,17 @@
 int main() {
     Employee E1, E2, E3;
 
-    
+    E1.get_data("Ali", 50000);
+    E2.get_data("Sara", 75000);
+    E3.get_data("Ahmed", 100000);
+
+    E1.display();
+    E2.display();
+    E3.display();
+
+    cout << "Updating tax to 3% for Ahmed\n";
+    E3.update_tax_percentage(3);
+    E3.display();
 
     return 0;
 }
