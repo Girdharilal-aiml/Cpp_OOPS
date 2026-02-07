8892522de9571930664c9cc355ca74ab7ef4b547
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-02-07 14:13:19 +0500

    Create Q18.cpp

---
+++ PRACTICE Q_A/Q18.cpp
@@ -0,0 +1,13 @@
+/*
+4. Create a CricketPlayer class with attributes:
+   - name
+   - jerseyNumber
+   - battingAverage
+
+   Requirements:
+   - Parameterized constructor using this pointer
+   - improveAverage(runs) → increase average, return object (method chaining)
+   - playMatch(runsScored) → update batting average
+   - displayPlayerStats() → show player info
+   - Test with players: Babar Azam, Muhammad Rizwan, Saim Ayub
+*/
