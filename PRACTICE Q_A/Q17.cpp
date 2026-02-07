e2a8c36c1d20dbc3185ae4e46835005b4bee5fb1
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-02-07 14:13:16 +0500

    Create Q17.cpp

---
+++ PRACTICE Q_A/Q17.cpp
@@ -0,0 +1,11 @@
+/*
+3. Create a SmartDevice class with attributes:
+   - deviceName
+   - type
+   - status (on/off)
+
+   Requirements:
+   - Parameterized constructor → initialize device
+   - Destructor → print farewell message when object is destroyed
+   - Create and delete objects to test destructor behavior
+*/
