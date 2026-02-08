f0701063497b15ad5dac1c7a2c6e1ae444667064
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-02-08 21:35:58 +0500

    Create Q19.cpp

---
+++ PRACTICE Q_A/Q19.cpp
@@ -0,0 +1,14 @@
+/*
+5. Create a FootballPlayer class with attributes:
+   - playerName
+   - position
+   - goalCount
+
+   Requirements:
+   - Default constructor → "Unknown Player", "Benchwarmer", goals = 0
+   - Parameterized constructor → set all values
+   - Copy constructor → duplicate player
+   - Constructor with default arguments → only playerName required,
+     position = "Midfielder", goalCount = 10
+   - Create players, update goals, and display profiles
+*/
