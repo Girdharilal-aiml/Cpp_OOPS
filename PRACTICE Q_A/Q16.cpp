a3ed386c2a95375b887deeb088c20ee1b41a177e
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-02-09 18:02:02 +0500

    Update Q16.cpp

--- PRACTICE Q_A/Q16.cpp
+++ PRACTICE Q_A/Q16.cpp
@@ -20,6 +20,15 @@ private:
    string Days[7];
    int CurrentDay;
 
+   void fillDays(){
+     Days[0] = "Sunday";
+     Days[1] = "Monday";
+     Days[2] = "Tuesday";
+     Days[3] = "Wednesday";
+     Days[4] = "Thursday";
+     Days[5] = "Friday";
+     Days[6] = "Saturday";
+   }
 
 public:
 
