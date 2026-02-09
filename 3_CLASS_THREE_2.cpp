3b20ed8d9db3cc700323ad1b7b27e1b3b5f10a7d
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-02-09 20:48:11 +0500

    Update 3_CLASS_THREE_2.cpp

---
+++ 3_CLASS_THREE_2.cpp
@@ -0,0 +1,14 @@
+// BankSystem.h
+// #ifndef MY_HEADER
+// #define MY_HEADER
+
+#pragma once
+#include"iostream"
+using namespace std;
+
+class SharedAccount{
+    public:
+    double* balance;
+
+    
+};
