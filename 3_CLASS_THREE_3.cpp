bd9da451db4d092d7efe1fab4850c630527e56ad
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-02-11 21:13:06 +0500

    Update 3_CLASS_THREE_3.cpp

--- 3_CLASS_THREE_3.cpp
+++ 3_CLASS_THREE_3.cpp
@@ -1,4 +1,13 @@
 //main.cpp
 #include "iostream"
-#include "3_CLASS_THREE_2.h"
+#include "3_CLASS_THREE_BankSystem.h"
 using namespace std;
+
+int main(){
+    cout << "Shallow Copy Constructor" << endl;
+    cout << "=====================================================================================" << endl;
+    SharedAccount S1(1000.00);
+
+
+}
+
