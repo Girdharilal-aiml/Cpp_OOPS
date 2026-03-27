9c545f6f46207b9753e927953afa5b464832001d
Girdhari lal <girdharilal.aiml@gmail.com>
2026-03-27 13:27:39 +0500

    Update Q22.cpp

--- PRACTICE Q_A/Q22.cpp
+++ PRACTICE Q_A/Q22.cpp
@@ -2,5 +2,8 @@
 Design a C++ program to model a hospital management system with the following requirements:
 Requirements:
 
+1. Abstract class MedicalStaff with pure virtual 
+   functions:  showRole() and computePay()
+
 
 */
