731d8a27ad9db9bd2fee55b6acf16df51d43634e
Girdhari lal <girdharilal.aiml@gmail.com>
2026-03-28 17:02:20 +0500

    Update Q23.cpp

--- PRACTICE Q_A/Q23.cpp
+++ PRACTICE Q_A/Q23.cpp
@@ -1,3 +1,6 @@
 // Practice Q&A: OOP Concepts in C++
 // This program demonstrates Abstraction, Encapsulation, Inheritance, and Polymorphism
 
+#include <iostream>
+#include <string>
+using namespace std;
